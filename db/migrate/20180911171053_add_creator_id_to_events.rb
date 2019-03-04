7d0f37cfb8254c7dc78d024bf887365ff1d27feb
Max Garber <bubblebooy@gmail.com>
2019-03-03 20:46:15 -0600

    updating stuff to redeploy to heroku

--- db/migrate/20180911171053_add_creator_id_to_events.rb
+++ db/migrate/20180911171053_add_creator_id_to_events.rb
@@ -1,6 +1,6 @@
 class AddCreatorIdToEvents < ActiveRecord::Migration[5.2]
   def change
-    add_column :events, :creator_id, :interger
+    add_column :events, :creator_id, :integer
     add_foreign_key :events, :users, column: :creator_id
   end
 end
